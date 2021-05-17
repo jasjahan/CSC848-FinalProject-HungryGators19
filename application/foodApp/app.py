9b173b544cb9edc8c9bc030b63d70e0e8ae43280
TinThuZarAye-mm <tinthuzaraye0912@gmail.com>
2021-05-17 16:09:44 -0700

    adding delivery driver page

--- application/foodApp/app.py
+++ application/foodApp/app.py
@@ -25,10 +25,10 @@ app.jinja_env.filters['quote_plus'] = lambda u: quote_plus(u)
 
 # for sqlalchemy
 DB_USER = 'root'
-DB_PASSWORD = 'Gurjot-98554'
+DB_PASSWORD = ''
 DB_HOST = 'localhost'
 DB_PORT = 3306
-DB_NAME = 'HungryGators-19'
+DB_NAME = 'DatabaseDB'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{}:{}@{}:{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
 
